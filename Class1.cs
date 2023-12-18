f4e8b288db87c71346e64578136e10aa3820a03f
forthelion <igorj1@live,com>
2023-12-18 15:06:23 -0800

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace matrix_calculator
+{
+    class Class1
+    {
+        //number checkers 
+        //   function should loop until user enters a valied number and return if user exits before it negavitve -1 marking for error 
+        // need to refactor
+        public static int IsItNumber()
+        {
+            int userNumber = 0;
+            bool exit = false;
+            while (!exit)
+            {
+                string userInput = Console.ReadLine();
+                //bool NumberTrue = int.TryParse(userInput, out a);
+                bool NumberTrue = int.TryParse(userInput, out int a);
+                if (NumberTrue)
+                {
+                    userNumber = int.Parse(userInput);
+                    return userNumber;
+                }
+            }
+            // should never reach here
+            return -1;
+        }
+        // ask users for row and then column
+        public static int[,] setupMatrix()
+        {
+            Console.WriteLine("enter number of row");
+            int row = IsItNumber();
+            Console.WriteLine("enter number of columns");
+            int column = IsItNumber();
+            int[,] matrix;
+
+            matrix = new int[row, column];
+
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    Console.WriteLine("enter number");
+                    matrix[i, j] = Convert.ToInt16(IsItNumber());
+                    printMatrix(matrix);// System.IndexOutOfRangeException: 'Index was outside the bounds of the array
+                }
+                printMatrix(matrix);
+            }
+            return matrix;
+        }
+        // add a exception here if there nothing so it will be zero 
+        public static void printMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+
+            int columns = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        public static List<int> infoMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+
+            int columns = matrix.GetLength(1);
+
+            List<int> info = new List<int>();
+            info.Add(rows);
+            info.Add(columns);
+            return info;
+
+        }
+        public static int[,] addMatrix(int[,] matrix1, int[,] matrix2,List<int> matrixinfo)
+        {
+            int[,] matrix;
+            // matrix[ (row = [matrixinfo[1]), (column = [matrixinfo[2]))
+            matrix = new int[matrixinfo[0], matrixinfo[1]];
+
+            for (int i = 0; i < matrixinfo[1]; i++)
+            {
+                for (int j = 0; j < matrixinfo[0]; j++)
+                {
+                    matrix[i, j] = matrix1[i,j]+matrix2[i,j];
+                    
+                }
+            }
+
+            return matrix;
+
+        }
+        public static int[,] minusMatrix(int[,] matrix1, int[,] matrix2, List<int> matrixinfo)
+        {
+            int[,] matrix;
+            // matrix[ (row = [matrixinfo[1]), (column = [matrixinfo[2]))
+            matrix = new int[matrixinfo[0], matrixinfo[1]];
+
+            for (int i = 0; i < matrixinfo[1]; i++)
+            {
+                for (int j = 0; j < matrixinfo[0]; j++)
+                {
+                    matrix[i, j] = matrix1[i, j] - matrix2[i, j];
+
+                }
+            }
+
+            return matrix;
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace matrix_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // loop until users quits program. The user quites the program by typing exit which turns The bool Exit value true stopping the loop. will give it more users tolerances 
+            bool Exit = false;
+            while (Exit == false)
+            {
+
+
+                
+
+                Console.WriteLine("Do you want to add? type +");
+
+                Console.WriteLine("Do you want to subtract? type -");
+
+                Console.WriteLine("Do you want to Reduced Row Echelon Form? type RREF");
+
+                Console.WriteLine("Do you want to do multiplication ? type *");
+
+                Console.WriteLine("Do you want to quit program? type exit");
+                string userInput = Console.ReadLine();
+                Console.WriteLine("You entered: " + userInput);
+                if (userInput == "*")
+                {
+                    //ask for matrix 1 to be filled out
+                    // ask for matrix 2 to be filled out 
+                    multiplication();
+                }
+                if (userInput == "+")
+                {
+                    // fill out matrix 
+                    int[,] matrix1 = Class1.setupMatrix();
+                    int[,] matrix2 = Class1.setupMatrix();
+                    Add(matrix1,matrix2);
+                }
+
+                if (userInput == "-")
+                {
+                    int[,] matrix1 = Class1.setupMatrix();
+                    int[,] matrix2 = Class1.setupMatrix();
+                    minus(matrix1,matrix2);
+                }
+
+                if (userInput == "RREF")
+                {
+                    // ask for matrix 1 to be filled out
+                    RREF();
+                }
+
+                if (userInput == "exit")
+                {
+                    Exit = true;
+
+                }
+                else
+                {
+                    Console.WriteLine("can you type again?");
+                }
+
+
+                
+            }
+        }
+
+        private static void Add(int[,] matrix1, int[,] matrix2)
+        {
+            List<int> infoM1 = Class1.infoMatrix(matrix1);
+            List<int> infoM2 = Class1.infoMatrix(matrix2);
+            if(infoM1[0]==infoM2[0] && infoM1[1] == infoM2[1])
+            {
+                int[,] matrix = Class1.addMatrix(matrix1, matrix2, infoM1);
+                Console.WriteLine("result of calculation");
+                Class1.printMatrix(matrix);    
+            }
+            else
+            {
+                Console.WriteLine("please reenter as matrix cant be added together");
+            }
+           
+        }
+
+        
+
+        private static void minus(int[,] matrix1, int[,] matrix2)
+        {
+            List<int> infoM1 = Class1.infoMatrix(matrix1);
+            List<int> infoM2 = Class1.infoMatrix(matrix2);
+            if (infoM1[0] == infoM2[0] && infoM1[1] == infoM2[1])
+            {
+                int[,] matrix = Class1.minusMatrix(matrix1, matrix2, infoM1);
+                Console.WriteLine("result of calculation");
+                Class1.printMatrix(matrix);
+            }
+            else
+            {
+                Console.WriteLine("please reenter as matrix cant be added together");
+            }
+
+        }
+
+        private static void multiplication()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void RREF()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+
+    
+
+
+
+
+
+  
+
+    
+}
